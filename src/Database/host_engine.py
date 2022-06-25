095b2e80a902cc224e90326d24da1ccf498c2f06
vyktorino <vyktor.photon@gmail.com>
2022-06-25 23:31:01 +0200

    tentative database and webscrape

--- src/Database/host_engine.py
+++ src/Database/host_engine.py
@@ -4,12 +4,27 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
 
 # connecting
-engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine("sqlite:///:memory:", echo=True)
 
 # declare mapping
 Base = declarative_base()
 
-class Tweet(Base):
-    __tablename__ = 'tweets'
-    
-    
+
+class ClassTweet(Base):
+    __tablename__ = "tweets"
+
+    id = Column(String, primary_key=True)
+    date = Column(String)
+    place = Column(String)
+    tweet = Column(String)
+
+    def __repr__(self):
+        return "<ClassTweet(date = '%s', place = '%s', tweet = '%s'" % (
+            self.date,
+            self.place,
+            self.tweet,
+        )
+
+
+if __name__ == "__main__":
+    print(Base.metadata.create_all(engine))
--- src/twitter_crawl.py
+++ src/twitter_crawl.py
@@ -1,12 +1,20 @@
+import datetime
+
+import pytz
 import twint
 import pandas as pd
 
+from pytz import timezone
+from datetime import datetime as dt
 
-class TwitterCrawl:
+from webscraping.src.database import host_engine
+
+
+class ClassTwitterCrawl:
     def __init__(self):
         pass
 
-    def get_scrape(self) -> pd.DataFrame:
+    def webscrape(self) -> pd.DataFrame:
         """Returns the dataframe from the scrape
 
         Args:
@@ -20,28 +28,46 @@ class TwitterCrawl:
 
         topic_for_search = ["Rénovation énergétique"]
         c.Search = topic_for_search
-        c.Limit = 100  # number of Tweets to scrape
-        c.Lang = "fr"
 
+        # Time
+        eu_paris = timezone("Europe/Paris")
+        paris_time = dt.now(eu_paris)
+
+        today = dt.utcnow().date()  # current date and time
+        c.Since = paris_time.strftime("%Y-%m-%d")
+
+        c.Limit = 10  # number of Tweets to scrape
+        c.Lang = "fr"
         c.Pandas = True
         c.Hide_output = True
         # c.Near = "Paris"
 
         twint.run.Search(c)
 
+        keep_columns = ["id", "date", "place", "tweet"]
 
-def column_names():
-    return twint.output.panda.Tweets_df.columns
+        webscrape_df = twint.output.panda.Tweets_df[keep_columns]
+
+        return webscrape_df
+
+    # def scrape_to_sql(self, scrape_df: pd.DataFrame) -> None:
+    #     """Writes the data into the SQLAlchemy database
+
+    #     Args:
+    #         scrape_df (pd.DataFrame): output of the get_scrape method
+    #     """
+    #     return scrape_df.to_sql("Tweets", host_engine.engine)
+
+    # def fetch_table(self) -> pd.DataFrame:
+    #     """Returns the dataframe from the database via the connexion
+
+    #     Returns:
+    #         pd.DataFrame: processed dataframe fetched from the database
+    #     """
+    #     return pd.read_sql("select * from Tweets", host_engine.engine)
 
 
-def twint_to_pd(columns):
-    return twint.output.panda.Tweets_df[columns]
-
-
-keep_columns = ["id", "date", "place", "near", "geo"]
-data = twint_to_pd(keep_columns)
-print(data.head())
-
-
-# tweets_df = twint.storage.panda.Tweets_df
-# print(tweets_df.columns)
+if __name__ == "__main__":
+    Crawl = ClassTwitterCrawl()
+    data = Crawl.webscrape()
+    print(data)
