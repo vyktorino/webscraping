8b7e67669faa7aa388b068d3e507bccfc38b61fe
vyktorino <vyktor.photon@gmail.com>
2022-07-03 14:30:22 +0200

    update analysis

--- webscrape/main.py
+++ webscrape/main.py
@@ -1,3 +1,6 @@
+import datetime
+from datetime import datetime
+
 import pandas as pd
 
 from twitter_scrape import ClassTwitterScrape
@@ -5,8 +8,18 @@ Scrape = ClassTwitterScrape()
 
 tweets_df = Scrape.fetch_dataframe()
 
-tweets_df['session'] = (tweets_df.sort_values('date')['time'].transform(lambda x: (x.diff() > '00:02:00').cumsum()))
+tweets_df['date'] = pd.to_datetime(tweets_df['date'])
+# create a session for every 5 min
+# tweets_df['session'] = (tweets_df.sort_values('date')['date'].transform(lambda x: (x.diff() > '00:05:00').cumsum()))
 
+# create a list of 5 min intervals from the start of df
+start = tweets_df.iloc[0,'date']
+end = tweets_df.iloc[-1, 'date']
+interval = (end-start)
+
+# delta = datetime.datetime.strptime(minutes = 5)
+# end = datetime.datetime.strptime()
 
 if __name__ == '__main__':
-    print(tweets_df.shape)
+    x = start, end, interval
+    print(x)
